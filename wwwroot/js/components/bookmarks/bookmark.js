eda0dc3938b7b0c67b3c3b56efaa244b64201f8a
Andreas Rex <rex199@gmail.com>
2021-12-07 20:49:03 +0100

    Showing both titlebookmarks and actor bookmarks

--- wwwroot/js/components/bookmarks/bookmark.js
+++ wwwroot/js/components/bookmarks/bookmark.js
@@ -1,5 +1,17 @@
-﻿define(['knockout', 'postman'], function (ko, postman) {
+﻿define(['knockout', 'postman','bookmarkService'], function (ko, postman, bs) {
     return function (params) {
 
+
+        let bookmarks = ko.observableArray([]);
+        let actorbookmarks = ko.observableArray([]);
+
+        bs.getBookmarks(bookmarks)
+        bs.getActorBookmarks(actorbookmarks)
+
+        return {
+
+            bookmarks,
+            actorbookmarks
+        };
     };
 });
--- wwwroot/js/services/bookmarkService.js
+++ wwwroot/js/services/bookmarkService.js
@@ -4,16 +4,31 @@ define([], () => {
         let param = {
             headers: {
                 "Content-Type": "application/json",
-                'Authorization': 'Bearer ' + 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IjkiLCJyb2xlIjoiQWRtaW4iLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3ZlcnNpb24iOiJWMy4xIiwibmJmIjoxNjM4ODczMjA0LCJleHAiOjE2Mzg4NzUwMDQsImlhdCI6MTYzODg3MzIwNH0.NSZ2J3wA72VV6T-RnBCSodqmuD0Illul-FBc-ckLzbc'
+                'Authorization': 'Bearer ' + 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IjEiLCJyb2xlIjoiQWRtaW4iLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3ZlcnNpb24iOiJWMy4xIiwibmJmIjoxNjM4OTA1MDE2LCJleHAiOjE2Mzg5MDY4MTYsImlhdCI6MTYzODkwNTAxNn0.YFDRQLSqmjXKsagSRP8mKCijYnz6WisDod8aBTetcgQ'
             }
         }
-        fetch("api/Bookmark/TitleBookmark/1", param)
+        fetch("/api/Bookmark/TitleBookmark/1", param)
             .then(response => response.json())
-            .then(json => console.log(json));
+            .then(json => callback(json));
 
     };
 
+    let getActorBookmarks = (callback) => {
+        let param = {
+            headers: {
+                "Content-Type": "application/json",
+                'Authorization': 'Bearer ' + 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IjEiLCJyb2xlIjoiQWRtaW4iLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3ZlcnNpb24iOiJWMy4xIiwibmJmIjoxNjM4OTA1MDE2LCJleHAiOjE2Mzg5MDY4MTYsImlhdCI6MTYzODkwNTAxNn0.YFDRQLSqmjXKsagSRP8mKCijYnz6WisDod8aBTetcgQ'
+            }
+        }
+        fetch("/api/Bookmark/ActorBookmark/1", param)
+            .then(response => response.json())
+            .then(json => callback(json));
+
+    };
+
+
     return {
-        getBookmarks
+        getBookmarks,
+        getActorBookmarks
     }
 });
