5a2fe3cbe0630ca45384a8f5ff070e38a0db9d7a
CloudProDk <59182385+CloudProDk@users.noreply.github.com>
2021-12-09 13:28:36 +0100

    feat(Frontend): :sparkles: Added auth to movieservice and databinding to rating

--- wwwroot/js/components/login/login.js
+++ wwwroot/js/components/login/login.js
@@ -5,21 +5,21 @@
         let password = ko.observable('123')
         let token = ko.observable('');
 
-        
+
         let login = () => {
-           as.authenticate(userName(), password(), token)
-           setTimeout(navigate, 1000)
-            
+            as.authenticate(userName(), password(), token)
+            setTimeout(navigate, 1000)
+
         }
 
         let navigate = () => {
-            if(token().token){
+            if (token().token) {
                 console.log(token())
                 vm.bearerToken(token().token)
                 vm.userName(token().userName)
                 vm.uconst(token().uconst)
                 vm.activeView('bookmark')
-            } else {console.log('error')}
+            } else { console.log('error') }
         }
 
 
--- wwwroot/js/services/movieService.js
+++ wwwroot/js/services/movieService.js
@@ -1,18 +1,14 @@
-define([], () => {
+define(['viewmodel'], (vm) => {
 
     let getMovies = (callback) => {
 
         let param = {
             headers: {
                 "Content-Type": "application/json",
-                'Authorization': 'Bearer ' + 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IjEiLCJyb2xlIjoiQWRtaW4iLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3ZlcnNpb24iOiJWMy4xIiwibmJmIjoxNjM4OTkwNjM3LCJleHAiOjE2Mzg5OTI0MzcsImlhdCI6MTYzODk5MDYzN30.E3ghSKRF3fK5wXucNslAkoekltgTwQ2nPwb-qD8mU3g'
+                'Authorization': 'Bearer ' + vm.bearerToken()
             }
         }
         fetch("api/titles?Page=10&PageSize=50", param)
-
-
-
-
             .then(response => response.json())
             .then(json => callback(json))
 
