50f43958677b1986902de1d8033f98a60c78a280
serubi <serubi@live.com>
2021-12-13 23:34:31 +0100

    Make single actor page look less bad 🙃

--- wwwroot/js/components/singleActor/singleActor.js
+++ wwwroot/js/components/singleActor/singleActor.js
@@ -1,20 +1,51 @@
-﻿define(['knockout', 'postman','viewmodel','actorService'], function (ko, postman, vm, as) {
+﻿define(['knockout', 'postman','viewmodel','actorService', 'movieService'], function (ko, postman, vm, as, ms) {
     return function (params) {
+        // console.log(vm.currentactor().nconst);
+        // console.log(vm.currentactor());
 
-        console.log(vm.currentactor().nconst);
-        console.log(vm.currentactor());
         let actornconst = vm.currentactor().nconst;
-
-
         let actorToBeShown = ko.observableArray([]);
+        let actorProfessions = ko.observableArray([]);
+        let actorTitles = ko.observableArray([]);
 
         as.getActorByNconst(actorToBeShown, actornconst);
 
+        let getProfessions = (callback, actor) => {
+          callback(actor().primaryprofession.split(','));
+        };
 
-        console.log(actorToBeShown());
-        
+        let getTitles = (callback, actor) => {
+          let titles = [];
+          actor().knownfortitles.split(',').forEach((tconst, index, array) => {
+            let title = ko.observableArray([]);
+            ms.getMovieByTconst(title, tconst);
+            title.subscribe(function() {
+              if (title().primarytitle) { // Validate that the title was found and has data
+                titles.push(title());
+                if (index === array.length - 1) {
+                  // Return if it's the last item
+                  callback(titles);
+                }
+              }
+            });
+          });
+        }
+
+        let goToTitleDetails = titleItem => {
+          vm.curTitle(titleItem);
+          vm.activeView('details');
+        };
+
+        actorToBeShown.subscribe(function() {
+          getProfessions(actorProfessions, actorToBeShown);
+          getTitles(actorTitles, actorToBeShown);
+        });
+
         return {
-            actorToBeShown
+            actorToBeShown,
+            actorProfessions,
+            actorTitles,
+            goToTitleDetails
         }
     };
 });
