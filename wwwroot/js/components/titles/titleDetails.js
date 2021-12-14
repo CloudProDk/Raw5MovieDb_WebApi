52da393468cef8b480f82d1ea68aab36bc10cf83
serubi <serubi@live.com>
2021-12-14 12:06:58 +0100

    Fix navigate to actor, add genre title count to homepage

--- wwwroot/js/components/titles/titleDetails.js
+++ wwwroot/js/components/titles/titleDetails.js
@@ -3,22 +3,30 @@ define(['knockout', 'postman', 'viewmodel', 'movieService'], function (ko, postm
     let title = ko.observable(null);
     let actors = ko.observable(null);
 
+    let navigateToActor = actor => {
+        vm.currentactor(actor)
+        vm.activeView('singleActor')
+    }
+
     if (vm.curTitle().url) {
         ms.getTitle(vm.curTitle().url, title);
         ms.getTitleActors(vm.curTitle().actors, actors);
     } else {
         ms.getMovieByTconst(title, vm.curTitle().tconst);
         setTimeout(loadActors, 1000);
-
     }
 
     function loadActors() {
         ms.getTitleActors(title().actors, actors);
+        actors.subscribe(function() {
+          console.log(actors());
+        });
     }
-    
+
     return {
       title,
-      actors
+      actors,
+      navigateToActor
     };
   };
 });
